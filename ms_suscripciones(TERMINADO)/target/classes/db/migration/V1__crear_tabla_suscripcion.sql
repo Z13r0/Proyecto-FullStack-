de94d26d62ee52df89390a93ac8fb7d073353f54
Z13r0 <se.cerdab@duocuc.cl>
2026-06-23 20:15:52 -0400

    Delete ms_suscripciones(TERMINADO) directory

--- ms_suscripciones(TERMINADO)/target/classes/db/migration/V1__crear_tabla_suscripcion.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Suscripcion (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id BIGINT NOT NULL,
-    plan_id VARCHAR(50) NOT NULL,
-    fecha_inicio DATE NOT NULL,
-    fecha_fin DATE,
-    estado VARCHAR(20) NOT NULL,
-    monto DOUBLE
-);
